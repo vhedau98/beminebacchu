f205b726989a29e4b27b3f0ba5d7d2625939828b
vhedau98 <50128971+vhedau98@users.noreply.github.com>
2026-02-08 23:47:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 let currentPage = 0;
 const pages = document.querySelectorAll(".page");
 
+function typeText(el) {
+  const text = el.innerHTML;
+  el.innerHTML = "";
+  let i = 0;
+  const timer = setInterval(() => {
+    el.innerHTML += text[i++];
+    if (i >= text.length) clearInterval(timer);
+  }, 20);
+}
+
 function nextPage() {
-  if (currentPage < pages.length - 1) {
-    pages[currentPage].classList.remove("active");
-    currentPage++;
-    pages[currentPage].classList.add("active");
-  }
+  const current = pages[currentPage];
+  current.classList.remove("active");
+  currentPage++;
+  const next = pages[currentPage];
+  next.classList.add("active");
+
+  const nextText = next.querySelector(".type");
+  if (nextText) typeText(nextText);
 }
 
 function sayYes() {
